9f2770e20d2a36bf302d43b7efe64112f37414d1
Matthew-eth724 <tokoniayakazi.eth@gmail.com>
2026-02-20 15:45:24 +0100

    cl

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,6 @@ const Footer = () => {
                     <div className="social-links">
                         <a href="#"><Facebook size={20} /></a>
                         <a href="#"><Instagram size={20} /></a>
-                        <a href="#"><Twitter size={20} /></a>
                     </div>
                 </div>
 
@@ -19,8 +18,8 @@ const Footer = () => {
                     <h4>Contact Us</h4>
                     <ul className="footer-contact">
                         <li><MapPin size={18} /> 123 Luxury Lane, Event Est.</li>
-                        <li><Phone size={18} /> +1 (555) 123-4567</li>
-                        <li><Mail size={18} /> hello@cannesplace.com</li>
+                        <li><Phone size={18} /> +234 812 345 6789</li>
+                        <li><Mail size={18} /> events@cannesplace.com</li>
                     </ul>
                 </div>
 
